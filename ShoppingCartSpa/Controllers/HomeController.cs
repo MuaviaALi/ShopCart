6f3cdebbb1d9863da521e1ba89aedfe61e46d29a
MuaviaALi <alimaan045@gmail.com>
2017-09-11 04:44:33 -0700

    demo

---
+++ ShoppingCartSpa/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ShoppingCartSpa.Controllers
+{
+    public class HomeController : ApiController
+    {
+        [Route("api/GetProducts")]
+        [HttpGet]
+        public List<Product> GetProducts()
+        {
+            var db = new ShopCartEntities();
+            db.Configuration.ProxyCreationEnabled = false;
+
+            return db.Products.ToList();
+        }
+    }
+}
